da0c6f219f38099ee8ee57a9969e1fe64787d895
Michael Miller <mmiller@esri.com>
2019-10-31 11:27:59 -0400

    cleaned up code added progressor

--- un_editor_solutions/__init__.py
+++ un_editor_solutions/__init__.py
@@ -80,49 +80,55 @@ class SelectRelated(object):
         if not self.pro_map:
             pro_map = project.activeMap
         else:
-            pro_map = project.listMaps(wildcard=self.map)[-1]
+            pro_map = project.listMaps(wildcard=self.pro_map)[-1]
         if not pro_map:
             print("Open and activate a map")
             return
-        # Loop through all the layers and table
-        # build a lookup to layers by CIM so subtype layers can be evaluated together
-        # Build a layer lookup to apply selection set against
-        cim_lookup = {}
+
+        # Dict to lookup layer by datasource
         datasource_lookup = {}
+        cims_processed = []
+        selection_info = {}
+        missing_related = set()
+        # Loop through all the layers and table
         layers = pro_map.listLayers()
         tables = pro_map.listTables()
+        # Set the progressor
+        arcpy.SetProgressor("step", "Reviewing selected features", 0, len(layers + tables), 1)
         for layer in layers + tables:
-            cim_def = layer.getDefinition('V2')
-            cim_lookup[cim_def.uRI] = layer
+            # Update the progressor label for layer
+            arcpy.SetProgressorLabel(f"Evaluating selection set on {layer.name}")
 
-            if isinstance(cim_def,
-                          (arcpy.cim.CIMVectorLayers.CIMFeatureLayer,
-                           arcpy.cim.CIMVectorLayers.CIMSubtypeGroupLayer,
-                           arcpy.cim.CIMVectorLayers.CIMStandaloneTable)):
-                if hasattr(cim_def, 'featureTable'):
-                    datasource_lookup.setdefault(cim_def.featureTable.dataConnection.dataset, []).append(layer)
-                elif hasattr(cim_def, 'dataConnection'):
-                    datasource_lookup.setdefault(cim_def.dataConnection.dataset, []).append(layer)
-
-        cims_processed = []
-        for layer in layers:
             cim_def = layer.getDefinition('V2')
+            # Add Tables and Feature layers to the datasource lookup
+            if hasattr(layer, 'isFeatureLayer') is False or layer.isFeatureLayer:
+                datasource_lookup.setdefault(layer.connectionProperties['dataset'], []).append(layer)
+
             # If the layer was already processed as part of a subtype layer, do not re-evaul
             if cim_def.uRI in cims_processed:
+                # Update the progressor position
+                arcpy.SetProgressorPosition()
                 continue
+
             # Get the related class and its foreign ID
             rc_info = self.rc_info(layer=layer)
             # if there is not relationship, move to next layer
             if not rc_info:
+                # Update the progressor position
+                arcpy.SetProgressorPosition()
                 continue
             parent_ids = set()
             if isinstance(cim_def, arcpy.cim.CIMVectorLayers.CIMSubtypeGroupLayer):
                 # If it is a subtype layer, check the selection of all sublayers and collect primary key of the features
-                for sub_layer_cim in cim_def.subtypeLayers:
-                    cims_processed.append(sub_layer_cim)
-                    if cim_lookup[sub_layer_cim].getSelectionSet() is not None:
+                for sub_layer in layer.listLayers():
+                    cim_def_sub = sub_layer.getDefinition('V2')
+                    # If the layer was already processed as part of a subtype layer, do not re-evaul
+                    if cim_def_sub.uRI in cims_processed:
+                        continue
+                    cims_processed.append(cim_def_sub.uRI)
+                    if sub_layer.getSelectionSet() is not None:
                         fields = list({v['OriginPrimary'].lower() for k, v in rc_info.items()})
-                        parent_ids.update({row[0] for row in arcpy.da.SearchCursor(cim_lookup[sub_layer_cim], fields)})
+                        parent_ids.update({row[0] for row in arcpy.da.SearchCursor(sub_layer, fields)})
 
             else:
                 # Collect the primary key of the features
@@ -134,18 +140,34 @@ class SelectRelated(object):
             if parent_ids:
                 print(f'{cim_def.name}: found {len(parent_ids)} parent ids.')
                 for k, v in rc_info.items():
-                    if k not in datasource_lookup:
-                        # Related class not in the map
-                        continue
-                    # TODO: What to do with multi layers
-                    related_layer = datasource_lookup[k][0]
-                    count = 0
-                    for where_clause in self.create_sql(related_layer, v['OriginForeign'], parent_ids):
-                        oids = [row[0] for row in arcpy.da.SearchCursor(related_layer, ['objectid'], where_clause)]
-                        related_layer.setSelectionSet(oidList=oids, method='UNION')
-                        count += len(oids)
-                    if count:
-                        arcpy.AddMessage(f'Selecting {count} rows in {related_layer.name}')
+                    selection_info.setdefault(k, {'field': v['OriginForeign'], 'ids': set()})['ids'].update(parent_ids)
+            # Update the progressor position
+            arcpy.SetProgressorPosition()
+
+        # Set the progressor
+        arcpy.SetProgressor("step", "Selecting related features", 0, len(selection_info.keys()), 1)
+        for k, v in selection_info.items():
+            # Update the progressor label for layer
+            arcpy.SetProgressorLabel(f"Selecting rows in {k}")
+            if k not in datasource_lookup:
+                # Related class not in the map
+                missing_related.add(k)
+                # Update the progressor position
+                arcpy.SetProgressorPosition()
+                continue
+            # TODO: What to do with multi layers
+            related_layer = datasource_lookup[k][0]
+            count = 0
+            for where_clause in self.create_sql(related_layer, v['field'], v['ids']):
+                oids = [row[0] for row in arcpy.da.SearchCursor(related_layer, ['objectid'], where_clause)]
+                related_layer.setSelectionSet(oidList=oids, method='UNION')
+                count += len(oids)
+            if count:
+                arcpy.AddMessage(f'Selecting {count} rows in {related_layer.name}')
+            # Update the progressor position
+            arcpy.SetProgressorPosition()
+        if missing_related:
+            arcpy.AddWarning(f'The following related classes are missing from the map: {missing_related}')
 
 
 if __name__ == '__main__':
