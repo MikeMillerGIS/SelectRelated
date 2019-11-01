7cb7f45f6d75db5812ba4e27160aee38ac0309ec
Michael Miller <mmiller@esri.com>
2019-11-01 18:04:41 -0400

    expose data map, fixes for multi rcs

--- editor_tools.pyt
+++ editor_tools.pyt
@@ -1,4 +1,8 @@
+import arcpy
+
 from un_editor_solutions import SelectRelated
+
+
 class Toolbox(object):
     def __init__(self):
         """Define the toolbox (the name of the toolbox is the name of the
@@ -30,7 +34,23 @@ class SelectRelatedRecords(object):
         """Modify the values and properties of parameters before internal
         validation is performed.  This method is called whenever a parameter
         has been changed."""
-        return True
+
+        relate_map = arcpy.Parameter(name='relate_map',
+                                     displayName='Layer Relationship Map',
+                                     datatype='GPValueTable',
+                                     direction='Input',
+                                     parameterType='Optional',
+                                     enabled=None,
+                                     category=None,
+                                     symbology=None,
+                                     multiValue=None)
+
+        relate_map.columns = [['GPFeatureLayer', 'Source Layer'], ['Field', 'Source Key Field'],
+                              ['GPFeatureLayer', 'Target Layer'], ['Field', 'Target Key Field']]
+        # "{C99D0042-EF42-4B04-8A0B-1A53F6DB67A6}" -- proportional (no + sign and no separator)
+        # "{1AA9A769-D3F3-4EB0-85CB-CC07C79313C8}"  # + sign and separator
+        relate_map.controlCLSID = "{1AA9A769-D3F3-4EB0-85CB-CC07C79313C8}"
+        return [relate_map]
 
     def updateMessages(self, parameters):
         """Modify the messages created by internal validation for each tool
--- un_editor_solutions/__init__.py
+++ un_editor_solutions/__init__.py
@@ -1,14 +1,16 @@
 from functools import lru_cache
-
+from typing import Dict
+from operator import itemgetter
 import arcpy
 import os
 import pathlib
 import re
 
+
 class SelectRelated(object):
-    def __init__(self, project='CURRENT', pro_map=None):
+    def __init__(self, project='CURRENT', map_name=None):
         self.project = project
-        self.pro_map = pro_map
+        self.map_name = map_name
         self.pattern = re.compile(r'L(\d+).*')
 
     @staticmethod
@@ -57,13 +59,13 @@ class SelectRelated(object):
 
     def rc_info(self, layer):
         rc_class_info = {}
+        print(layer)
         desc = arcpy.Describe(layer)
         if hasattr(desc, 'FeatureClass') is False or desc.FeatureClass.dataType != 'FeatureClass':
             return rc_class_info
         if not desc.relationshipClassNames:
             return rc_class_info
-        client_server = not str(desc.path).startswith('http')
-        if not client_server:
+        if str(desc.path).startswith('http'):
             workspace = desc.path
             workspace_desc = self.describe_workspace(workspace.lower())
             workspace_lookup = {child.name: child for child in workspace_desc.children}
@@ -83,6 +85,8 @@ class SelectRelated(object):
                     layer_id = self.pattern.findall(cls)[0]
                     if layer_id == desc.name:
                         continue
+                    # rc_class_info.setdefault(layer_id, []).append(
+                    #    {cl_key[1]: cl_key[0] for cl_key in rc_desc.originClassKeys})
                     rc_class_info[layer_id] = {cl_key[1]: cl_key[0] for cl_key in rc_desc.originClassKeys}
         else:
             workspace = self.get_workspace(desc.catalogPath)
@@ -102,19 +106,37 @@ class SelectRelated(object):
                 for cls in rc_desc.destinationClassNames:
                     if cls == desc.name:
                         continue
+                    # rc_class_info.setdefault(cls, []).append(
+                    #    {cl_key[1]: cl_key[0] for cl_key in rc_desc.originClassKeys})
                     rc_class_info[cls] = {cl_key[1]: cl_key[0] for cl_key in rc_desc.originClassKeys}
 
         return rc_class_info
 
-    def main(self):
+    @staticmethod
+    def merge_dols(dol1, dol2):
+        # https://stackoverflow.com/questions/1495510/combining-dictionaries-of-lists-in-python
+        keys = set(dol1).union(dol2)
+        no = []
+        return dict((k, dol1.get(k, no) + dol2.get(k, no)) for k in keys)
+
+    @staticmethod
+    def rows_as_dicts(cursor):
+        col_names = cursor.fields
+        for row in cursor:
+            yield dict(zip(col_names, row))
+
+    def main(self, relate_map):
         # get the current project and map
         project = arcpy.mp.ArcGISProject(self.project)
-        if not self.pro_map:
+        pro_map = None
+        if not self.map_name:
             pro_map = project.activeMap
         else:
-            pro_map = project.listMaps(wildcard=self.pro_map)[-1]
+            maps = project.listMaps(wildcard=self.map_name)
+            if maps:
+                pro_map = maps[-1]
         if not pro_map:
-            print("Open and activate a map")
+            print("Map could not be found")
             return
 
         # Dict to lookup layer by datasource
@@ -128,6 +150,9 @@ class SelectRelated(object):
         # Set the progressor
         arcpy.SetProgressor("step", "Reviewing selected features", 0, len(layers + tables), 1)
         for layer in layers + tables:
+            if layer.isBroken:
+                arcpy.AddWarning(f'{layer} is broken')
+                continue
             # Update the progressor label for layer
             arcpy.SetProgressorLabel(f"Evaluating selection set on {layer.name}")
 
@@ -135,23 +160,29 @@ class SelectRelated(object):
             # Add Tables and Feature layers to the datasource lookup
             if hasattr(layer, 'isFeatureLayer') is False or layer.isFeatureLayer:
                 datasource_lookup.setdefault(layer.connectionProperties['dataset'], []).append(layer)
+                datasource_lookup.setdefault(layer.name, []).append(layer)
 
             # If the layer was already processed as part of a subtype layer, do not re-evaul
             if cim_def.uRI in cims_processed:
                 # Update the progressor position
                 arcpy.SetProgressorPosition()
                 continue
-
-            # Get the related class and its foreign ID
-            rc_info = self.rc_info(layer=layer)
+            if relate_map:
+                if layer.name not in relate_map:
+                    continue
+                rc_info = relate_map[layer.name]
+            else:
+                # Get the related class and its foreign ID
+                rc_info = self.rc_info(layer=layer)
             # if there is not relationship, move to next layer
             if not rc_info:
                 # Update the progressor position
                 arcpy.SetProgressorPosition()
                 continue
-            parent_ids = set()
+            parent_ids = {}
             if isinstance(cim_def, arcpy.cim.CIMVectorLayers.CIMSubtypeGroupLayer):
                 # If it is a subtype layer, check the selection of all sublayers and collect primary key of the features
+                fields = list({v['OriginPrimary'].lower() for k, v in rc_info.items()})
                 for sub_layer in layer.listLayers():
                     cim_def_sub = sub_layer.getDefinition('V2')
                     # If the layer was already processed as part of a subtype layer, do not re-evaul
@@ -159,20 +190,24 @@ class SelectRelated(object):
                         continue
                     cims_processed.append(cim_def_sub.uRI)
                     if sub_layer.getSelectionSet() is not None:
-                        fields = list({v['OriginPrimary'].lower() for k, v in rc_info.items()})
-                        parent_ids.update({row[0] for row in arcpy.da.SearchCursor(sub_layer, fields)})
-
+                        with arcpy.da.SearchCursor(sub_layer, fields) as cursor:
+                            ids = dict(zip(fields, zip(*cursor)))
+                            for field in fields:
+                                parent_ids.setdefault(field, []).extend(list(ids[field]))
             else:
                 # Collect the primary key of the features
                 cims_processed.append(cim_def.uRI)
                 if layer.getSelectionSet() is not None:
-                    fields = [v['OriginPrimary'] for k, v in rc_info.items()]
-                    parent_ids = {row[0] for row in arcpy.da.SearchCursor(layer, fields)}
-
-            if parent_ids:
-                print(f'{cim_def.name}: found {len(parent_ids)} parent ids.')
+                    fields = list({v['OriginPrimary'].lower() for k, v in rc_info.items()})
+                    with arcpy.da.SearchCursor(layer, fields) as cursor:
+                        parent_ids = dict(zip(fields, zip(*cursor)))
+            # As the result is a collection of values by field, get a arbitrary field and count its values for a count
+            select_count = len(next(iter(parent_ids.values())))
+            if select_count:
+                print(f'{cim_def.name}: found {select_count} parent ids.')
                 for k, v in rc_info.items():
-                    selection_info.setdefault(k, {'field': v['OriginForeign'], 'ids': set()})['ids'].update(parent_ids)
+                    selection_info.setdefault(k, {'field': v['OriginForeign'], 'ids': set()})['ids'].update(
+                        parent_ids[v['OriginPrimary'].lower()])
             # Update the progressor position
             arcpy.SetProgressorPosition()
 
@@ -194,8 +229,7 @@ class SelectRelated(object):
                 oids = [row[0] for row in arcpy.da.SearchCursor(related_layer, ['objectid'], where_clause)]
                 related_layer.setSelectionSet(oidList=oids, method='UNION')
                 count += len(oids)
-            if count:
-                arcpy.AddMessage(f'Selecting {count} rows in {related_layer.name}')
+            arcpy.AddMessage(f'Selecting {count} rows in {related_layer.name}')
             # Update the progressor position
             arcpy.SetProgressorPosition()
         if missing_related:
@@ -203,5 +237,22 @@ class SelectRelated(object):
 
 
 if __name__ == '__main__':
-    SelectRelated(project="C:\_MyFiles\github\SelectRelateRecords\sample\SelectRelated.aprx",
-                  pro_map="Electric_FGDB").main()
+    relate_map_test = {
+        'Electric Device': {
+            'ElectricDeviceUnit': {
+                'OriginPrimary': 'GlobalID',
+                'OriginForeign': 'relateddevice'
+            }}}
+    relate_map_test = {
+        'Electric Device': {
+            'Device Unit': {
+                'OriginPrimary': 'GlobalID',
+                'OriginForeign': 'relateddevice'
+            }}}
+    map_name = "Electric_FGDB"
+    map_name = "Electric_FGDB_all"
+    map_name = "Electric Network Editor"
+
+    relate_map_test = relate_map_test
+    SelectRelated(project=r"C:\_MyFiles\github\SelectRelateRecords\sample\SelectRelated.aprx",
+                  map_name=map_name).main(relate_map=relate_map_test)
